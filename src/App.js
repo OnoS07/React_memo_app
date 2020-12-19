2194ab80557c7909b00bfaef1e1b15c6dcc82ba6
大野颯一郎 <soichrooono@gmail.com>
2020-12-19 21:57:08 +0900

    [add]TodoList.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import TodoList from './TodoList'
 
 class App extends React.Component {
-  render() {
-    return (
-      <h1>TODOアプリ</h1>
-    );
+    render() {
+      return (
+        <div>
+          <h1>TODOアプリ</h1>
+          <TodoList />
+        </div>
+      );
+    }
   }
-}
-
-export default App;
+  
+  export default App;
---
+++ src/TodoList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TodoList(){
+    return(
+        <>
+            <div>プログラミング</div>
+            <div>お買い物</div>
+        </>
+    );
+}
+
+export default TodoList;
