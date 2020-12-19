ad608c68581de9601f08fa6150f1f7c0ea0e0d38
大野颯一郎 <soichrooono@gmail.com>
2020-12-19 22:22:29 +0900

    [add]todolist_List

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
 function TodoList(){
     return(
-        <>
-            <div>プログラミング</div>
-            <div>お買い物</div>
-        </>
-    );
-}
+        <List>
+            <ListItem>
+                <ListItemText primary="プログラミング"></ListItemText>
+            </ListItem>
+            <ListItem>
+                <ListItemText primary="お買い物"></ListItemText>
+            </ListItem>
+        </List>
+    )
+};
 
 export default TodoList;
